17fe4c1af194eb816bd88bdc1a1e14d1d04a711a
safvanhameed5 <safvanhameed5@gmail.com>
2024-09-14 15:09:27 +0530

    Implemented Next Icon

--- src/Components/Pagination.js
+++ src/Components/Pagination.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { MdNavigateNext } from "react-icons/md";
+import { GrFormPrevious } from "react-icons/gr";
 
 const Pagination = ({ page, setPage, totalPages }) => {
   return (
@@ -8,7 +10,7 @@ const Pagination = ({ page, setPage, totalPages }) => {
         className="p-2 bg-gray-300 rounded-l hover:bg-gray-400 transition"
         disabled={page === 1}
       >
-        Previous
+        <GrFormPrevious />
       </button>
 
       <span className="p-2 bg-white border border-gray-300">
@@ -20,7 +22,7 @@ const Pagination = ({ page, setPage, totalPages }) => {
         className="p-2 bg-gray-300 rounded-r hover:bg-gray-400 transition"
         disabled={page === totalPages}
       >
-        Next
+        <MdNavigateNext />
       </button>
     </div>
   );
